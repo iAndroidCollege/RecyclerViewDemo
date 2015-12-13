e585b5a105ecb74ac24c0ce7e907ab22f84b2b38
iAndroidCollege <collegeandroid2015@gmail.com>
2015-12-13 19:20:02 +0200

    Added Images And Android Versions. Fixed Layout Change

--- app/src/main/java/college/edu/tomer/recyclerviewdemo/RecyclerViewDemoActivity.java
+++ app/src/main/java/college/edu/tomer/recyclerviewdemo/RecyclerViewDemoActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -18,8 +19,8 @@ public class RecyclerViewDemoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view_demo);
-/*        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);*/
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         RecyclerView rv = (RecyclerView) findViewById(R.id.rvAndroidVersion);
         initDummyData();
